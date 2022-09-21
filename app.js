de7e4ada84b4658b5b32f85f1dfe6b62c3843df9
Vlăduță Octavian <99560158+OctavianVladuta@users.noreply.github.com>
2022-09-21 23:32:25 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+const speechRecognitionService = window.SpeechRecognition || window.webkitSpeechRecognition
+const recognitionService = new speechRecognitionService();
+
+const startBtn = document.querySelector(".btn-start");
+const textLog = document.querySelector(".text-log");
+
+const languages ={
+    English:"en-US",
+    Romanian:"ro-RO"
+}
+
+startBtn.addEventListener("click",()=>{
+recognitionService.lang=determineLanguage();
+recognitionService.continuous = true;
+
+recognitionService.onresult = handleResult;
+
+
+if(startBtn.classList.contains("btn-pulsating")){  
+    recognitionService.stop();
+    startBtn.classList.remove("btn-pulsating");
+    textLog.innerHTML+="<br />"
+}   else{
+recognitionService.start();
+startBtn.classList.add("btn-pulsating");
+}
+
+
+
+});
+
+function determineLanguage(){
+    const selected = document.querySelector("#language").value;
+    switch(selected){
+        case "English": 
+            return languages.English;
+        case "Romanian":
+            return languages.Romanian;
+        default:
+            throw new Error("Language not supported");
+    }
+}
+
+function handleResult(event){
+    const results = [];
+    for(const result of event.results){
+        results.push(`${result[0].transcript}`);
+    }
+    textLog.innerHTML+= results.at(-1);
+}
